3d402ef12198c97d5a188121158b959e61c78979
Rohit Valanki <rval00008@student.monash.edu>
2024-05-28 00:49:58 +1000

    Fixed mobile view css

--- centerstar-automotive/src/components/NavigationBar.jsx
+++ centerstar-automotive/src/components/NavigationBar.jsx
@@ -4,9 +4,9 @@ import logo from "../img/logo.png";
 
 function NavigationBar() {
     return (
-        <div className="bg-nav-color w-full h-20 flex items-center p-8 shadow-2xl">
-            <img src={logo} alt="logo" className="w-52"/>
-            <section className="ml-auto flex flex-row gap-4">
+        <div className="bg-nav-color w-full flex flex-col sm:flex-row items-center justify-between p-3 sm:p-4"> {/* Adjust padding */}
+            <img src={logo} alt="logo" className="w-48 mb-1 sm:mb-0"/> {/* Adjust margin */}
+            <section className="flex flex-row gap-3 sm:w-auto sm:pl-4"> {/* Add padding */}
                 <Button btnText="About Us" type="hover:bg-highlight-color hover:text-nav-color hover:cursor-pointer text-highlight-color"/>
                 <Button btnText="Contact Us" type="hover:bg-nav-color hover:text-highlight-color hover:cursor-pointer bg-highlight-color"/>
             </section>
