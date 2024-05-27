31c4a64479c70055e4aec7cf906db90059d1c922
nethbtju <neth.b.ss@gmail.com>
2024-05-28 00:02:38 +1000

    Added logo

--- centerstar-automotive/src/components/NavigationBar.jsx
+++ centerstar-automotive/src/components/NavigationBar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Button from "./Button";
+import logo from "../img/logo.png";
 
 function NavigationBar() {
     return (
         <div className="bg-nav-color w-full h-20 flex items-center p-8">
+            <img src={logo} alt="logo" className="w-52"/>
             <section className="ml-auto flex flex-row gap-4">
                 <Button btnText="About Us" type="hover:bg-highlight-color hover:text-nav-color hover:cursor-pointer text-highlight-color border-solid border-2 border-highlight-color"/>
                 <Button btnText="Contact Us" type="hover:bg-nav-color hover:text-highlight-color hover:cursor-pointer border-solid border-2 border-highlight-color bg-highlight-color"/>
