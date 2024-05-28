909e5a54efbd0d7cecc0ce0c89efb783bc0e173f
Rohit Valanki <rval00008@student.monash.edu>
2024-05-28 10:53:30 +1000

    Added basic card elements

--- centerstar-automotive/src/components/Card.jsx
+++ centerstar-automotive/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ children }) => {
   return (
-    <div className="bg-gray-900 rounded-md p-4 w-1/5 h-48"> 
+    <div className="bg-card-grey-color rounded-md p-4 w-1/4 h-48 shadow_md"> 
       {children}
     </div>
   );
--- centerstar-automotive/src/pages/HomePage.jsx
+++ centerstar-automotive/src/pages/HomePage.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import Card from "../components/Card";
 
 function HomePage() {
   return (
-    <div className="w-full sm:w-[50%] bg-bg-grey-color bg-opacity-50 flex justify-center">
-      <h1 className="font-inria font-bold text-white pt-16 text-3xl sm:text-5xl text-center">
+    <div className="w-full sm:w-[50%] bg-bg-grey-color bg-opacity-50">
+      <h1 className="font-inria font-bold text-white pt-16 pb-6 text-3xl sm:text-5xl text-center">
         Mercedes Benz Specialist
       </h1>
+
+      {/* First row of cards */}
+      <div className="flex justify-center pt-8 space-x-4"> {/* Added space-x-4 for horizontal spacing */}
+        <Card />
+        <div className="px-2"></div> {/* Added horizontal padding */}
+        <Card />
+        <div className="px-2"></div> {/* Added horizontal padding */}
+        <Card />
+      </div>
+
+      {/* Second row of cards */}
+      <div className="flex justify-center pt-8 space-x-4"> {/* Added space-x-4 for horizontal spacing */}
+        <Card />
+        <div className="px-2"></div> {/* Added horizontal padding */}
+        <Card />
+        <div className="px-2"></div> {/* Added horizontal padding */}
+        <Card />
+      </div>
     </div>
   );
 }
