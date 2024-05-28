bb97f996cdaf0a12e6ccb561719c01c84543a164
nethbtju <neth.b.ss@gmail.com>
2024-05-28 21:59:52 +1000

    extending screen

--- centerstar-automotive/src/pages/HomePage.jsx
+++ centerstar-automotive/src/pages/HomePage.jsx
@@ -9,29 +9,36 @@ import card6 from "../img/card-assets/card6.jpg";
 
 function HomePage() {
   return (
-    <div className="w-full sm:w-[70%] bg-bg-grey-color bg-opacity-50">
-      <h1 className="font-inria font-bold text-white pt-16 pb-6 text-3xl sm:text-4xl text-center">
-        Mercedes Benz Specialist
-      </h1>
+      <div className="w-full sm:w-[70%] bg-bg-grey-color bg-opacity-60">
+          <h1 className="font-inria font-bold text-white pt-16 pb-6 text-3xl sm:text-4xl text-center">
+              Mercedes Benz Specialist
+          </h1>
 
-      {/* First row of cards */}
-      <div className="flex justify-center pt-8 space-x-4">
-        <Card imageUrl={card1} title="Repairs" /> {/* Pass card1 image and title */}
-        <div className="px-2"></div>
-        <Card imageUrl={card2} title="Diagnostic" /> {/* Pass card2 image and title */}
-        <div className="px-2"></div>
-        <Card imageUrl={card3} title="Air Conditioning" /> {/* Pass card3 image and title */}
-      </div>
+          {/* First row of cards */}
+          <div className="flex justify-center pt-8 space-x-4">
+              <Card imageUrl={card1} title="Repairs"/> {/* Pass card1 image and title */}
+              <div className="px-2"></div>
+              <Card imageUrl={card2} title="Diagnostic"/> {/* Pass card2 image and title */}
+              <div className="px-2"></div>
+              <Card imageUrl={card3} title="Air Conditioning"/> {/* Pass card3 image and title */}
+          </div>
 
-      {/* Second row of cards */}
-      <div className="flex justify-center pt-8 space-x-4">
-        <Card imageUrl={card4} title="Electrical" /> {/* Pass card4 image and title */}
-        <div className="px-2"></div>
-        <Card imageUrl={card5} title="Services" /> {/* Pass card5 image and title */}
-        <div className="px-2"></div>
-        <Card imageUrl={card6} title="And more..." /> {/* Pass card6 image and title */}
+          {/* Second row of cards */}
+          <div className="flex justify-center pt-8 space-x-4">
+              <Card imageUrl={card4} title="Electrical"/> {/* Pass card4 image and title */}
+              <div className="px-2"></div>
+              <Card imageUrl={card5} title="Services"/> {/* Pass card5 image and title */}
+              <div className="px-2"></div>
+              <Card imageUrl={card6} title="And more..."/> {/* Pass card6 image and title */}
+          </div>
+          <div className="flex justify-center pt-8 space-x-4">
+              <Card imageUrl={card4} title="Electrical"/> {/* Pass card4 image and title */}
+              <div className="px-2"></div>
+              <Card imageUrl={card5} title="Services"/> {/* Pass card5 image and title */}
+              <div className="px-2"></div>
+              <Card imageUrl={card6} title="And more..."/> {/* Pass card6 image and title */}
+          </div>
       </div>
-    </div>
   );
 }
 
