67dce42dca29bf94793502532eb82719d2ac0625
nethbtju <neth.b.ss@gmail.com>
2024-05-28 21:31:02 +1000

    image add

--- centerstar-automotive/src/components/Card.jsx
+++ centerstar-automotive/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ children, imageUrl, title }) => {
   return (
     <div className="bg-card-grey-color rounded-md w-1/4 shadow-md relative overflow-hidden transform transition-transform hover:scale-105">
       <div className="h-11/20 w-full"> {/* Set height to 55% of the card */}
-        <img src={imageUrl} alt="Card Image" className="w-full h-full object-cover" /> {/* Set image to fill its container */}
+        <img src={imageUrl} alt="Card Image" className="w-full h-36 overflow-hidden object-cover" /> {/* Set image to fill its container */}
       </div>
       <div className="h-9/20 w-full p-4"> {/* Reduced vertical padding and increased horizontal padding */}
         <h2 className="font-inter text-white text-lg mb-2" style={{ fontWeight: 450 }}>{title}</h2> {/* Set font weight manually */}
